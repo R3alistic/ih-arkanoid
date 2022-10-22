877acb8b0011c54f2e23e6ed3c87b6bbdf5273d7
R3alistic <acunhax@gmail.com>
2022-10-22 15:29:52 +0100

    js major overhaul

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let canvas = document.getElementById("gameArea");
 let ctx = canvas.getContext('2d');
-
+//Ball Variables
 let ballRadius = 6;
-let x = canvas.width/2;
-let y = canvas.height-30;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 let dx = 2
-let dy = -2 ;
+let dy = -2;
+//Paddle Variables
 let paddleHeight = 20;
 let paddleWidth = 85;
-let paddleX = (canvas.width-paddleWidth)/2;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = y;
 let rightPressed = false;
 let leftPressed = false;
+let lastPaddleX = paddleX;
+//Brick Variables
 let brickRowCount = 13;
 let brickColumnCount = 6;
 let brickWidth = 55;
@@ -18,9 +22,36 @@ let brickHeight = 20;
 let brickPadding = 4;
 let brickOffsetTop = 100;
 let brickOffsetLeft = 17;
+//UI Variables
 let score = 0;
 let lives = 3;
-
+//PowerUp Variables, Class and Type Object
+let powerUpCount = 2;
+let powerUpSpeed = 0.1;
+let powerUpChance = 0.0001;
+//Reset Powerups when game starts
+let powerUpSuper = false;
+let powerUpLife = false;
+let powerUpSticky = false;
+let powerUpExtension = false;
+class PowerUp {
+    constructor(x, y, width, height, type) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.type = type;
+        this.yv = powerUpSpeed * height;
+    }
+}
+const powerUpType = {
+    extend: { color: "blue" },
+    life: { color: "red" },
+    sticky: { color: "cyan" },
+    super: { color: "magenta" }
+}
+//Array for bricks and PowerUps
+let powerUpsArray = [];
 let bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
@@ -29,6 +60,7 @@ for (let c = 0; c < brickColumnCount; c++) {
     }
 }
 
+//Event listeners for player pad control
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
@@ -64,15 +96,17 @@ function collisionDetection() {
             let b = bricks[c][r];
             if (b.status == 1) {
                 if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-                    dy = -dy;
+                    //if ball is super it will not bounce
+                    if (!powerUpSuper) {
+                        dy = -dy;
+                    }
                     b.status = 0;
                     score++;
-
+                    //Hit collision sound
                     let ballTouchSound;
                     ballTouchSound = new Audio("./soundEffects/ballTouchSound.wav");
                     ballTouchSound.play();
-
-
+                    //Win condition
                     if (score == brickRowCount * brickColumnCount) {
                         alert("YOU WIN, CONGRATS!");
                         document.location.reload();
@@ -85,98 +119,163 @@ function collisionDetection() {
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#C0C0C0";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    //if condition for super powerup ball and sticky powerup ball
+    if (powerUpSuper) {
+        ctx.fillStyle = powerUpType.super.color;
+    }
+    else if (powerUpSticky) {
+        ctx.fillStyle = powerUpType.sticky.color;
+    }
+    else {
+        ctx.fillStyle = "silver";
+    }
     ctx.fill();
     ctx.closePath();
 }
 
 function drawPaddle() {
     ctx.beginPath();
-    paddle = new Image();
-    paddle.src ="./images/paddleimage.png";
-    ctx.drawImage(paddle, paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    let paddle = new Image();
+    //if condition for sticky power up
+    paddle.src = powerUpSticky ? "./images/magnetic-pad.png" : "./images/paddle-image.png";
+    ctx.drawImage(paddle, paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
     ctx.closePath();
+    //Power Up hit collision with player paddle
+    for (let i = powerUpsArray.length - 1; i >= 0; i--) {
+        if (powerUpsArray[i].x + powerUpsArray[i].width * 0.5 > paddleX - paddleWidth * 0.5
+            && powerUpsArray[i].x - powerUpsArray[i].width * 0.5 < paddleX + paddleWidth * 0.5
+            && powerUpsArray[i].y + powerUpsArray[i].height * 0.5 > paddleY - paddleHeight * 0.5
+            && powerUpsArray[i].y + powerUpsArray[i].height * 0.5 < paddleY + paddleHeight * 0.5) {
+            switch (powerUpsArray[i].type) {
+                //extend power up
+                case powerUpType.extend:
+                    powerUpExtension = true;
+                    paddleWidth = 115;
+                    break;
+                //life incrementation
+                case powerUpType.life:
+                    lives++;
+                    break;
+                //activate ball stickyness
+                case powerUpType.sticky:
+                    powerUpSticky = true;
+                    powerUpSuper = false;
+                    break;
+                //activate super ball
+                case powerUpType.super:
+                    powerUpSuper = true;
+                    powerUpSticky = false;
+                    break;
+            }
+            powerUpsArray.slice(i, 1);
+            //Add powerup sound here
+        }
+    }
 }
 
 function drawBricks() {
-  for (let c = 0; c < brickColumnCount; c++) {
-    for (let r = 0; r < brickRowCount; r++) {
-      if (bricks[c][r].status == 1) {
-        let brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
-        let brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        switch (c) {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
+            let brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            //creates new PowerUp
+            if (bricks[c][r].status == 0 && Math.random() < powerUpChance) {
+                let px = bricks[c][r].x + brickWidth / 2;
+                let py = bricks[c][r].y + brickHeight / 2;
+                let pWidth = brickWidth / 2;
+                let pHeight = brickHeight / 3;
+                let pKeys = Object.keys(powerUpType)
+                let pKey = pKeys[Math.floor(Math.random() * pKeys.length)];
+                powerUpsArray.push(new PowerUp(px, py, pWidth, pHeight, powerUpType[pKey]));
+            }
+            //creates the bricks
+            else if (bricks[c][r].status == 1) {
+                switch (c) {
+                    case 0:
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "silver";
+                        ctx.strokeStyle = "grey";
+                        ctx.fill();
+                        ctx.closePath();
+                        break;
 
-          case 0:
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "silver";
-            ctx.strokeStyle = "grey";
-            ctx.fill();
-            ctx.closePath();
-            break;
+                    case 1:
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "red";
+                        ctx.fill();
+                        ctx.closePath();
+                        break;
 
-          case 1:
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "red";
-            ctx.fill();
-            ctx.closePath();
-            break;
+                    case 2:
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "yellow";
+                        ctx.fill();
+                        ctx.closePath();
+                        break;
 
-          case 2:
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "yellow";
-            ctx.fill();
-            ctx.closePath();
-            break;
+                    case 3:
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "blue";
+                        ctx.fill();
+                        ctx.closePath();
+                        break;
 
-          case 3:
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "blue";
-            ctx.fill();
-            ctx.closePath();
-            break;
+                    case 4:
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "magenta";
+                        ctx.fill();
+                        ctx.closePath();
+                        break;
 
-          case 4:
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "magenta";
-            ctx.fill();
-            ctx.closePath();
-            break;
-
-          case 5:
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "green";
-            ctx.fill();
-            ctx.closePath();
-            break;
+                    case 5:
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "green";
+                        ctx.fill();
+                        ctx.closePath();
+                        break;
+                }
+            }
         }
-      }
     }
-  }
+}
+
+function drawPowerUps() {
+    ctx.fillStyle = "cyan";
+    powerUpsArray.forEach((powerUp) => {
+        ctx.fillRect(powerUp.x, powerUp.y, powerUp.width, powerUp.height);
+    })
+}
+
+function updatePowerUps() {
+    for (let i = powerUpsArray.length - 1; i >= 0; i--) {
+        powerUpsArray[i].y += powerUpsArray[i].yv;
+        // delete powerup when off canvas
+        if (powerUpsArray[i].y - powerUpsArray[i].h * 0.5 > canvas.height) {
+            powerUpsArray.splice(i, 1);
+        }
+    }
 }
 
 function drawScore() {
-  ctx.font = "20px Nordic Light";
-  ctx.fillStyle = "white";
-  ctx.fillText("Score: " + score, 18, 30);
+    ctx.font = "20px Nordic Light";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: " + score, 18, 30);
 }
-
-let paddleLife = new Image();
-paddleLife.src ="./images/lifeSymbol.png";
 
 function drawLives() {
     ctx.font = "20px Nordic Light";
@@ -185,12 +284,7 @@ function drawLives() {
 }
 
 
-function clear(){
-    ctx.clearRect(0,0,c.width,c.height);
-}
-
 function draw() {
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
     drawBall();
@@ -198,78 +292,98 @@ function draw() {
     drawScore();
     drawLives();
     collisionDetection();
-    
+    drawPowerUps();
+    updatePowerUps();
 
-    if (lives === 3) {
-        ctx.drawImage(paddleLife, canvas.width - 50, 20, 40, 20);
-        ctx.drawImage(paddleLife, canvas.width - 90, 20, 40, 20);
-        ctx.drawImage(paddleLife, canvas.width - 130, 20, 40, 20);
-    } else if (lives === 2) {
-        ctx.drawImage(paddleLife, canvas.width - 90, 20, 40, 20);
-        ctx.drawImage(paddleLife, canvas.width - 130, 20, 40, 20);
-    } else if (lives === 1) {
-        ctx.drawImage(paddleLife, canvas.width - 130, 20, 40, 20);
-    } 
+    //draw additional life icons depending on life variable
+    let paddleLife = new Image();
+    paddleLife.src = "./images/life-symbol.png";
+    switch (lives) {
+        case 1:
+            ctx.drawImage(paddleLife, canvas.width - 130, 20, 40, 20);
+            break;
 
+        case 2:
+            ctx.drawImage(paddleLife, canvas.width - 90, 20, 40, 20);
+            ctx.drawImage(paddleLife, canvas.width - 130, 20, 40, 20);
+            break;
 
+        default:
+            ctx.drawImage(paddleLife, canvas.width - 50, 20, 40, 20);
+            ctx.drawImage(paddleLife, canvas.width - 90, 20, 40, 20);
+            ctx.drawImage(paddleLife, canvas.width - 130, 20, 40, 20);
+            break;
+    }
+
+    //bounce off canvas walls
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if (y + dy < ballRadius) {
         dy = -dy;
     }
+    //bounce off player paddle
     else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
+            if (powerUpSticky) {
+                dx = 0;
+                dy = 0;
+            }
+            else {
+                dy = -dy;
+            }
+            //Add magnetic noise file here?
         }
+        //life decrement
         else {
             lives--;
-
-            if(!lives) {
+            //game over condition
+            if (!lives) {
                 alert("Sorry Loser, Game over");
                 document.location.reload();
             }
-
+            //reset ball, player and powerups when life lost and not game over
             else {
-                x = canvas.width/2;
-                y = canvas.height-30;
+                x = canvas.width / 2;
+                y = canvas.height - 30;
                 dx = 2;
                 dy = -2;
-                paddleX = (canvas.width-paddleWidth)/2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+                powerUpSuper = false;
+                powerUpLife = false;
+                powerUpSticky = false;
+                powerUpExtension = false;
             }
         }
     }
-
-
+    //if conditions to move player paddle AND stop pad from leaving canvas game area
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
     }
     else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
     }
-
-    
+    // move the stationary ball with the paddle when sticky power up is online
+    else if (dy == 0) {
+        //x += paddleX - lastPaddleX;
+        x = paddleX + paddleWidth * 0.5;
+        y = paddleY - paddleHeight;
+    }
     x += dx;
     y += dy;
-    
     requestAnimationFrame(draw);
 }
-
 draw();
 
 function startGame() {
-
     let startDiv = document.getElementById("start");
     let gameCanvas = document.getElementById("gameArea");
     startDiv.style.display = "none";
     gameCanvas.style.display = "block";
-
-    
+    //startup sound
     let startSound;
     startSound = new Audio("./soundEffects/gameStartSound.wav");
     startSound.play();
-    
-
     draw();
 }
 
